a5e6d4d9d07d5bc362905a2303ac9f0cc01e4f40
madebyannie <angysantos2008@gmail.com>
2026-09-08 19:24:25 -0500

    Update sw.js

--- calculadora/sw.js
+++ calculadora/sw.js
@@ -1,5 +1,5 @@
 const CACHE = "annie-costos-v1";
-const FILES = ["./made_by_annie_costos.html","./manifest.json","./sw.js","./icon.svg"];
+const FILES = ["."./index.html","./manifest.json","./sw.js","./icon.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
 self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
