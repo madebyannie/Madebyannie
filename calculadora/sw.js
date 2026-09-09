90ad468d35bad0ee8ec451dc0865820046189bdb
madebyannie <angysantos2008@gmail.com>
2026-09-08 21:42:40 -0500

    Add files via upload

--- calculadora/sw.js
+++ calculadora/sw.js
@@ -1,25 +1,5 @@
-const CACHE = "annie-costos-v2";
-
-const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./sw.js",
-  "./icon.svg"
-];
-
-self.addEventListener("install", e =>
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  )
-);
-
-self.addEventListener("activate", e =>
-  e.waitUntil(self.clients.claim())
-);
-
-self.addEventListener("fetch", e =>
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  )
-);
+const CACHE = "annie-costos-v1";
+const FILES = ["./index.html","./manifest.json","./sw.js","./icon.svg"];
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
